8938ed1ba059c17c491ddd0b3e9582ee067c5ddc
lysvm <lysenkovm@yandexlyceum.ru>
2022-11-18 18:32:18 +0600

    Third commit

--- program.py
+++ program.py
@@ -1,5 +1,7 @@
 def main():
     print('My first git program')
     print('Add I change it every day')
+    print('Again')
+    print('UFO came and added this ')
 if __name__ == '__main__':
     main()
